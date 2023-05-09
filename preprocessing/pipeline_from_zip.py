1ee7caf7d054fd9c3674f3261331dbb9a234aa37
Bingguang LU <u6592909@anu.edu.au>
2023-05-09 14:31:28 +1000

    change preprocessing step

--- preprocessing/pipeline_from_zip.py
+++ preprocessing/pipeline_from_zip.py
@@ -1,8 +1,10 @@
 '''
 Refer to official document: https://docs.python.org/3/library/zipfile.html#zipfile-objects and 
 online resources: https://stackoverflow.com/questions/40824807/reading-zipped-json-files
+simplejson package idea from: https://stackoverflow.com/questions/1960516/python-json-serialize-a-decimal-object
 '''
 import ijson
+import simplejson as json
 from zipfile import ZipFile
 import os
 
@@ -15,26 +17,24 @@ out = open('../mnt/preprocessed_zip.json', 'a')
 out.write('{\"data\": [')
 count = 1
 
+
 with ZipFile('../twitter-huge.json.zip', 'r') as zip:
     with zip.open(zip.namelist()[0]) as f:
-        items = ijson.items(f, 'rows.item.doc')
-        for item in items:
-            preprocessed_dict = {}
-            for record in items:
-                try:
-                    id = record['data']['author_id']
-                    place = record['includes']['places'][0]['full_name'].split(',')[0].lower()
-                    js_out = '{\"author_id\": \"' + id + '\", \"place\": \"' + place + '\"}'
-                    if count != 1:
-                        out.write(', \n')
-                    out.write(js_out)
-                    count += 1
-                except:
-                    continue
-                # if count == 10:
-                    # break
-            # if count == 10:
-                # break
+        items = ijson.items(f, 'rows.item')
+        for record in items:
+            try:
+                id = record['doc']['data']['author_id']
+                place = record['doc']['includes']['places'][0]['full_name']
+                js_out = json.dumps(record)
+                if count != 1:
+                    out.write(', \n')
+                out.write(js_out)
+                count += 1
+            except:
+                continue
+            #if count == 100:
+                #break
+
 
 out.write('], \"count\": \"' + str(count) + '\"}')          
 out.close()  
--- preprocessing/test_output.py
+++ preprocessing/test_output.py
@@ -1,6 +1,6 @@
 import json
 
-with open('../mnt/preprocessed.json', 'r') as f:
+with open('../mnt/preprocessed_zip.json', 'r') as f:
     item = json.load(f)
     
 print(item['count'])
