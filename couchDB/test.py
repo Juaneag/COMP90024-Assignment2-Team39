f078bbcd3f7ebc77f9b7dea82f002ca6ae56abf6
Bingguang LU <u6592909@anu.edu.au>
2023-05-11 03:53:49 +1000

    ansible build instances to create couchdb cluster

---
+++ couchDB/test.py
@@ -0,0 +1,13 @@
+import couchdb
+import ijson
+
+couch = couchdb.Server()
+
+couch = couchdb.Server('http://admin:admin@172.26.136.129:5984/')
+db = couch.create('twitter') 
+
+db = couch["twitter"]
+with open('../mnt/preprocessed_zip.json') as f:
+    items = ijson.items(f, 'data.item')
+    for record in items:
+        db.save(record)
